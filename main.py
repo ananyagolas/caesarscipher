9679d3d731b36683ce707a047bbafa53b6d23a54
ananya-g <31283886-ananya-g@users.noreply.replit.com>
2025-07-23 19:10:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#This program is the starter code for the Programming Caesar's Cipher Project. 
+# This code is inspired by Cracking Codes with Python: An Introduction to Building and Breaking Ciphers by Al Sweigart https://www.nostarch.com/crackingcodes (BSD Licensed)
+
+# Global variables
+possibleCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# Run code
+
+# Introduction
+print("Welcome! This program will encrypt or decrypt your secret message using the Caesar cipher. \n\nWhen creating your message, you may only choose from the following characters: " + possibleCharacters + "\n\nLet's get started!\n")
+
+# Receive user input
+
+# Encrypt or decrypt the message
+
+# Print the shifted message
